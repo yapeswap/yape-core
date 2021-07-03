12ebb8db007fe9e8f56a999dfaa9bfa234da6f89
0xMayflower <0xMayflower@protonmail.com>
2021-07-04 00:12:25 +0900

    Use @workhard/utils instead of YapeFeeManager.sol

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,35 +1,35 @@
-import { task } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle";
-import '@typechain/hardhat';
+import { task } from 'hardhat/config'
+import '@nomiclabs/hardhat-waffle'
+import '@typechain/hardhat'
+import 'hardhat-dependency-compiler'
 
-require("dotenv").config();
+require('dotenv').config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (args, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
   solidity: {
-    version: "0.8.6",
+    version: '0.8.6',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 999999
-      }
-    }
+        runs: 999999,
+      },
+    },
   },
   networks: {
     hardhat: {
@@ -38,15 +38,20 @@ export default {
       mining: {
         auto: true,
       },
-      forking: process.env.FORK_URL ? {
-        url: process.env.FORK_URL,
-      } : undefined,
+      forking: process.env.FORK_URL
+        ? {
+            url: process.env.FORK_URL,
+          }
+        : undefined,
     },
   },
   typechain: {
     outDir: 'src/types',
     target: 'ethers-v5',
     alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
-    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules) 
-  }
-};
+    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+  },
+  dependencyCompiler: {
+    paths: ['@workhard/utils/contracts/fee/FeeManager.sol'],
+  },
+}
