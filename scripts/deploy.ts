2f6673dab7eae6663e9bf9b3296817ef15f67973
0xMayflower <0xMayflower@protonmail.com>
2021-07-05 21:46:38 +0900

    deploy contracts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,7 @@
 export const MAINNET_DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
 export const MAINNET_WEHT9 = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
 export const MAINNET_YEARN_V2_REGISTRY = `0x50c1a2ea0a861a967d9d0ffe2ae4012c2e053804`
+export const WHF_DIVIDEND_POOL = `0xe2Bc819B52AE921248F44B2d5c2dDE93b731Ce4e`
 
 import hre from 'hardhat'
 
@@ -8,12 +9,17 @@ async function main() {
   const [signer] = await hre.ethers.getSigners()
   const YapeFactory = (await hre.ethers.getContractFactory('YapeFactory')).connect(signer)
   const YapePairCode = (await hre.ethers.getContractFactory('YapePairCode')).connect(signer)
-  const FeeManager = (await hre.ethers.getContractFactory('YapePairCode')).connect(signer)
   const YapeRouter = (await hre.ethers.getContractFactory('UniswapV2Router02')).connect(signer)
+  const FeeManager = (await hre.ethers.getContractFactory('FeeManager')).connect(signer)
 
   const pairCode = await YapePairCode.deploy()
   const factory = await YapeFactory.deploy(signer.address, MAINNET_YEARN_V2_REGISTRY, signer.address, pairCode.address)
-  const feeManager = await FeeManager.deploy()
+  const feeManager = await FeeManager.deploy(
+    signer.address,
+    '0xe2Bc819B52AE921248F44B2d5c2dDE93b731Ce4e',
+    MAINNET_DAI,
+    MAINNET_WEHT9
+  )
   const yapeRouter = await YapeRouter.deploy(factory.address, MAINNET_WEHT9)
   console.log('PairCode deployed to:', pairCode.address)
   console.log('Factory deployed to:', factory.address)
